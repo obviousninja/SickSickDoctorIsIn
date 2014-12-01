f9daa62bb24abaf62913db2b164d09475eda0237
pkem3192 <onlyforplaying1192>
2014-12-01 14:07:36 -0500

    Added MainActivity and Profile Activity
    The main activity and profile activity has been added to the main file
    "TheDoctorIsIn" I copied my strings xml tags into the strings.xml. I
    copied my icons into one of the drawable folders. I overwrote the old
    android manifest with the one I made. I added my layouts and menu to the
    appropriate folders.

--- TheDoctorIsIn/src/com/example/thedoctorisin/MainActivity.java
+++ TheDoctorIsIn/src/com/example/thedoctorisin/MainActivity.java
@@ -1,16 +1,77 @@
-package com.example.thedoctorisin;
+package com.example.doctorisin;
 
 import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.registrationone);
+		setContentView(R.layout.activity_main);
+		
+		ImageView food_view = (ImageView) findViewById(R.id.food_view);
+		ImageView entertainment_view = (ImageView) findViewById(R.id.entertainment_view);
+		ImageView personal_view = (ImageView) findViewById(R.id.personal_view);
+		ImageView sports_view = (ImageView) findViewById(R.id.sports_view);
+		ImageView religion_view = (ImageView) findViewById(R.id.religion_view);
+		ImageView other_view = (ImageView) findViewById(R.id.other_view);
+		
+		food_view.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// Go to food question list
+			}
+		});
+		
+		entertainment_view.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// Go to entertainment question list
+			}
+		});
+		
+		personal_view.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// Go to personal question list 	
+			}
+		});
+		
+		sports_view.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// Go to sports question list 
+			}
+		});
+		
+		religion_view.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// Go to religion question list 
+			}
+		});
+		
+		other_view.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// Go to other question list 
+			}
+		});
+		
+		
 	}
 
 	@Override
@@ -26,7 +87,14 @@ public class MainActivity extends ActionBarActivity {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+		if (id == R.id.my_profile) {
+			//Go to Profile Page
+			Intent profileIntent = new Intent(MainActivity.this,ProfileActivity.class);
+			startActivity(profileIntent);
+			return true;
+		}
+		if(id == R.id.ask_a_question){
+			//Go to ask a question page
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
---
+++ TheDoctorIsIn/src/com/example/thedoctorisin/ProfileActivity.java
@@ -0,0 +1,26 @@
+package com.example.doctorisin;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class ProfileActivity extends Activity{
+	//These are the text fields which should be edited according to profile data
+	TextView coin_edit, asked_edit, answered_edit;
+	
+	//A list of all the user's currently open questions. The user should be able to
+	//click on the question and check if it has been answered or not.
+	ListView open_question_list;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.profile_layout2);
+		
+		coin_edit = (TextView) findViewById(R.id.coin_edit);
+		asked_edit = (TextView) findViewById(R.id.asked_edit);
+		answered_edit = (TextView) findViewById(R.id.answered_edit);
+		open_question_list = (ListView) findViewById(R.id.open_question_list);
+	}
+}
