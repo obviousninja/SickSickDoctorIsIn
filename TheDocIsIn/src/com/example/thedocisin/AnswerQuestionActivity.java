3d34b07e6e6ca61628624a8fb760e10f182260eb
pkem3192 <onlyforplaying1192>
2014-12-04 18:58:16 -0500

    go change jing

---
+++ TheDocIsIn/src/com/example/thedocisin/AnswerQuestionActivity.java
@@ -0,0 +1,46 @@
+package com.example.thedocisin;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class AnswerQuestionActivity extends Activity {
+
+	TextView questionText;
+	EditText answer;
+	Button cancel, submit;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_answer);
+		
+		questionText = (TextView) findViewById(R.id.answerQuestionText);
+		answer = (EditText) findViewById(R.id.answerAnswerEditText);
+		cancel = (Button) findViewById(R.id.answerCancelButton);
+		submit = (Button) findViewById(R.id.answerSubmitButton);
+		
+		cancel.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				finish();
+			}
+		});
+		
+		submit.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				if (answer.getText().toString().length() < 2) {
+					Toast.makeText(getApplicationContext(), "Answer needs to be longer", Toast.LENGTH_LONG).show();
+				} else {
+					//Add answer to wherever it needs to go
+					finish();
+				}
+			}
+		});
+	}
+}
