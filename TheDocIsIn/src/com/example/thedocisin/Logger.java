67a8eadccd55b0896e26e541002db61685c71915
obviousninja <infinityarc@gmail.com>
2014-12-07 13:39:54 -0500

    sharepref updated

--- TheDocIsIn/src/com/example/thedocisin/Logger.java
+++ TheDocIsIn/src/com/example/thedocisin/Logger.java
@@ -3,6 +3,7 @@ package com.example.thedocisin;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,12 +12,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+
 public class Logger extends Activity {
 
 	static final int REGISTRATION_REQUEST = 1; //flag for registration for result intent
 	Context mContext;
 	String userInputName;
 	String userInputPass;
+	private static String USER_NAME = 	"username";
+	
+	//SharePreferences curPreference = Activity.getPreferences(MODE_PRIVATE);
+	//SharePreferenceEditor = SharePreferences.edit();
 	
 	private HTTPServicesTask serviceHelper;
 	
@@ -25,13 +31,25 @@ public class Logger extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_logger);
 		
+		final SharedPreferences sharePrf = getPreferences(MODE_PRIVATE);
+		
+		
 		mContext = getApplicationContext();
 		serviceHelper = HTTPServicesTask.getInstance(mContext);
 		serviceHelper.setLogger(this);
 		
+		//if sharepref is not a null, then use that on the edit text field of nickname
+		EditText logger = (EditText) findViewById(R.id.username);
+		if(sharePrf != null){
+		if(sharePrf.getString(USER_NAME, "").isEmpty() == false){
+			logger.setText(sharePrf.getString(USER_NAME, ""));
+		}
+		}
+		
 		Button login = (Button) findViewById(R.id.loginButton);
 		login.setOnClickListener(new View.OnClickListener() {
 			
+		
 			
 			@Override
 			public void onClick(View v) {
@@ -41,9 +59,17 @@ public class Logger extends Activity {
 				EditText userp = (EditText) findViewById(R.id.password);
 				userInputPass = userp.getText().toString();
 				
+				
+				
 				//TODO checking if the users exist or not
 				serviceHelper.verifyUserPass(userInputName, userInputPass);
 				
+				//change the current user_name to something different, otherwise keep it as it is
+					if(sharePrf == null || sharePrf.getString(USER_NAME, "").isEmpty() || sharePrf.getString(USER_NAME, "").compareToIgnoreCase(userInputName) != 0){
+				SharedPreferences.Editor editor = sharePrf.edit();
+				editor.putString(USER_NAME, userInputName);
+				editor.commit();
+				}
 				
 				//TODO if user exist, start the intent, otherwise display a toast
 				//saying user does not exist
@@ -106,10 +132,14 @@ public class Logger extends Activity {
 	
 	
 	public void verifyUserPass(boolean verified){
+		
 		if(verified){
 			Toast.makeText(mContext, "Logging in " + userInputName, Toast.LENGTH_SHORT).show();
 			serviceHelper.setCurrentUser(userInputName);
-
+			
+		
+			
+			
 			Intent loggedIntent = new Intent(mContext, MainActivity.class);
 			startActivity(loggedIntent);	
 		}else{
